aae6e636a0c4dea3475aea8d9e220cf7cba5fea1
Jerry <exnxs@hotmail.com>
2017-06-14 11:22:08 -0400

    Create  TodoApp component

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+
+  render: function(){
+    return (
+      <div>
+        <h1>TodoApp</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
